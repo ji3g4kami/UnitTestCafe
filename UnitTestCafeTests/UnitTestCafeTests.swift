f099b3cbcff90827b73cf9982ac20a1b82c4040d
Deng Li <ji3g4kami@gmail.com>
2018-06-18 17:58:25 +0800

    Failed Unit Testing segue

--- UnitTestCafeTests/UnitTestCafeTests.swift
+++ UnitTestCafeTests/UnitTestCafeTests.swift
@@ -11,26 +11,26 @@ import XCTest
 
 class UnitTestCafeTests: XCTestCase {
     
+    var orderController: DVOrderViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        orderController = UIStoryboard(name: "Order", bundle: nil).instantiateViewController(withIdentifier:String(describing: DVOrderViewController.self)) as! DVOrderViewController
+        orderController.customizedItems = [
+            CustomizedItem(name: "Tea", iced: true, sugar: false)
+        ]
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        orderController = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSegues() {
+        orderController.confirmButtonPressed(ConfirmCell())
+        guard let receivedItems = orderController.nextController?.customizedItems else { return }
+        XCTAssertEqual(receivedItems[0].name, orderController.customizedItems[0].name)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }
